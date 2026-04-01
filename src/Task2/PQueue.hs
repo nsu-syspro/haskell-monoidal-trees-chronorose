c44320a35444f8d852c6b91ff721612f6ddce8fb
chronorose <rendemare@gmail.com>
2026-04-01 21:14:26 +0700

    task 1 and task 2 done

--- src/Task2/PQueue.hs
+++ src/Task2/PQueue.hs
@@ -1,32 +1,52 @@
 {-# OPTIONS_GHC -Wall #-}
+
 -- The above pragma enables all warnings
 
 module Task2.PQueue where
 
+import Common.MonoidalTree
 import Common.PriorityQueue
-
-import Task1 (Measured(..), MinMax(..))
+import Data.Bifunctor (Bifunctor (second))
+import Task1 (Measured (..), MinMax (..))
 import Task2.Tree
 
 -- * Priority queue definition
 
 -- | Priority queue based on binary tree
-newtype PQueue k v = PQueue { getTree :: Tree (MinMax k) (Entry k v) }
+newtype PQueue k v = PQueue {getTree :: Tree (MinMax k) (Entry k v)}
   deriving (Show, Eq)
 
 -- | Priority queue entry wrapper
-newtype Entry k v = Entry { getEntry :: (k, v) }
+newtype Entry k v = Entry {getEntry :: (k, v)}
   deriving (Show, Eq)
 
-instance Ord k => Measured (MinMax k) (Entry k v) where
-  measure = error "TODO: define measure (Measured (MinMax k) (Task2.Entry k v))"
+instance (Ord k) => Measured (MinMax k) (Entry k v) where
+  measure (Entry (k, _)) = measure k
 
 -- * Priority queue instance
 
+treeExtractMin :: forall k v. (Ord k) => Tree (MinMax k) (Entry k v) -> Maybe (v, Tree (MinMax k) (Entry k v))
+treeExtractMin Empty = Nothing
+treeExtractMin (Leaf a) = Just ((snd . getEntry) a, Empty)
+treeExtractMin (Branch m l r) =
+  let (leftMin, _) = getMinMax ((measure :: Tree (MinMax k) (Entry k v) -> MinMax k) l)
+      (centralMin, _) = getMinMax m
+      tree = if leftMin == centralMin then treeExtractMin l else treeExtractMin r
+   in fmap (\(v, t) -> (v, if leftMin == centralMin then branch t r else branch l t)) tree
+
+treeExtractMax :: forall k v. (Ord k) => Tree (MinMax k) (Entry k v) -> Maybe (v, Tree (MinMax k) (Entry k v))
+treeExtractMax Empty = Nothing
+treeExtractMax (Leaf a) = Just ((snd . getEntry) a, Empty)
+treeExtractMax (Branch m l r) =
+  let (_, rightMax) = getMinMax ((measure :: Tree (MinMax k) (Entry k v) -> MinMax k) r)
+      (_, centralMax) = getMinMax m
+      tree = if rightMax == centralMax then treeExtractMin r else treeExtractMin l
+   in fmap (\(v, t) -> (v, if rightMax == centralMax then branch l t else branch t r)) tree
+
 instance PriorityQueue PQueue where
-  empty = error "TODO: define empty (PriorityQueue Task2.PQueue)"
-  toPriorityQueue = error "TODO: define toPriorityQueue (PriorityQueue Task2.PQueue)"
-  entries = error "TODO: define entries (PriorityQueue Task2.PQueue)"
-  insert = error "TODO: define insert (PriorityQueue Task2.PQueue)"
-  extractMin = error "TODO: define extractMin (PriorityQueue Task2.PQueue)"
-  extractMax = error "TODO: define extractMax (PriorityQueue Task2.PQueue)"
+  empty = PQueue Empty
+  toPriorityQueue = foldr (uncurry insert) empty
+  entries = foldr ((:) . getEntry) [] . getTree
+  insert k v (PQueue tree) = PQueue (Entry (k, v) <| tree)
+  extractMin = fmap (second (foldr (uncurry insert . getEntry) empty)) . treeExtractMin . getTree
+  extractMax = fmap (second (foldr (uncurry insert . getEntry) empty)) . treeExtractMax . getTree
--- src/Task2/Seq.hs
+++ src/Task2/Seq.hs
@@ -1,42 +1,79 @@
 {-# OPTIONS_GHC -Wall #-}
+
 -- The above pragma enables all warnings
 
 module Task2.Seq where
 
+-- import Common.MonoidalTree
 import Common.Sequence
-
-import Task1 (Measured(..), Size(..))
+import Data.Foldable (Foldable (foldl'))
+import Task1 (Measured (..), Size (..))
 import Task2.Tree
 
 -- * Sequence definition
 
 -- | Random-access sequence based on binary tree
-newtype Seq a = Seq { getTree :: Tree (Size a) (Elem a) }
+newtype Seq a = Seq {getTree :: Tree (Size a) (Elem a)}
   deriving (Show, Eq)
 
 -- | Sequence element wrapper
-newtype Elem a = Elem { getElem :: a }
+newtype Elem a = Elem {getElem :: a}
   deriving (Show, Eq)
 
 -- | Measures given element as 'Size 1'
 instance Measured (Size a) (Elem a) where
-  measure = error "TODO: define measure (Measured (Size a) (Task2.Elem a))"
+  measure = const $ Size 1
 
 instance Foldable Seq where
-  foldMap = error "TODO: define foldMap (Foldable Task2.Seq)"
+  foldMap f a = foldMap (f . getElem) (getTree a)
 
   -- An O(1) implementation of length is possible
   -- due to size of the tree being cached at each node
   length :: forall a. Seq a -> Int
-  length = error "TODO: define length (Foldable Task2.Seq)"
+  length = getSize . (measure :: Tree (Size a) (Elem a) -> Size a) . getTree
 
 -- * Sequence instance
 
+-- TODO: generalize, beautify, simplify
+treeInsertAt :: Int -> a -> Tree (Size a) (Elem a) -> Tree (Size a) (Elem a)
+treeInsertAt _ v Empty = (leaf . Elem) v
+treeInsertAt ind v (Leaf a) = if ind <= 0 then branch (leaf $ Elem v) (Leaf a) else branch (Leaf a) (leaf $ Elem v)
+treeInsertAt ind v (Branch _ l r) =
+  let leftMeasure :: Int
+      leftMeasure = (getSize . (measure :: Tree (Size a) (Elem a) -> Size a)) l
+   in if ind <= leftMeasure
+        then branch (treeInsertAt ind v l) r
+        else branch l (treeInsertAt (ind - leftMeasure) v r)
+
+treeElemAt :: Int -> Tree (Size a) (Elem a) -> Maybe a
+treeElemAt _ Empty = Nothing
+treeElemAt ind (Leaf a) = if ind == 0 then (Just . getElem) a else Nothing
+treeElemAt ind (Branch _ l r) =
+  let leftMeasure :: Int
+      leftMeasure = (getSize . (measure :: Tree (Size a) (Elem a) -> Size a)) l
+   in if ind < leftMeasure
+        then treeElemAt ind l
+        else treeElemAt (ind - leftMeasure) r
+
+treeRemoveAt :: Int -> Tree (Size a) (Elem a) -> Tree (Size a) (Elem a)
+treeRemoveAt _ Empty = Empty
+treeRemoveAt ind (Leaf a) = if ind == 0 then Empty else Leaf a
+treeRemoveAt ind (Branch _ l r) =
+  let leftMeasure :: Int
+      leftMeasure = (getSize . (measure :: Tree (Size a) (Elem a) -> Size a)) l
+   in if ind < leftMeasure
+        then case treeRemoveAt ind l of
+          Empty -> r
+          tree -> branch tree r
+        else case treeRemoveAt (ind - leftMeasure) r of
+          Empty -> l
+          tree -> branch l tree
+
 instance Sequence Seq where
-  empty = error "TODO: define empty (Sequence Task2.Seq)"
-  toSequence = error "TODO: define toSequence (Sequence Task2.Seq)"
-  (+|) = error "TODO: define (+|) (Sequence Task2.Seq)"
-  (|+) = error "TODO: define (|+) (Sequence Task2.Seq)"
-  insertAt = error "TODO: define insertAt (Sequence Task2.Seq)"
-  removeAt = error "TODO: define removeAt (Sequence Task2.Seq)"
-  elemAt = error "TODO: define elemAt (Sequence Task2.Seq)"
+  empty = Seq Empty
+  toSequence = foldl' (|+) empty
+  (+|) = insertAt 0
+  (|+) sq = flip (insertAt (length sq)) sq
+  insertAt ind v = Seq . treeInsertAt ind v . getTree
+  removeAt ind = Seq . treeRemoveAt ind . getTree
+  elemAt ind = treeElemAt ind . getTree
